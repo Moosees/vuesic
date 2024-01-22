becd1efb78cbbd28c10b244860df77e5875bdc1c
Moosees <linus.almgren@gmail.com>
2024-01-22 22:41:21 +0100

    add AppAuth component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
 	<app-header></app-header>
+	<app-auth></app-auth>
 </template>
 
 <script>
 import AppHeader from "./components/AppHeader.vue";
+import AppAuth from "./components/AppAuth.vue";
 
 export default {
 	name: 'App',
 	components: {
-		AppHeader
+		AppHeader,
+		AppAuth
 	}
 };
 </script>
