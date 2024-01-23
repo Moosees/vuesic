c2abb8f67073eb4e6c0dc98d2ce10be363d5b7d7
Moosees <linus.almgren@gmail.com>
2024-01-23 23:41:37 +0100

    add open auth modal functionality

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="fixed z-10 inset-0 overflow-y-auto hidden" id="modal">
+	<div class="fixed z-10 inset-0 overflow-y-auto" id="modal" :class="hiddenClass">
 		<div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
 			<div class="fixed inset-0 transition-opacity">
 				<div class="absolute inset-0 bg-gray-800 opacity-75"></div>
@@ -113,3 +113,15 @@
 		</div>
 	</div>
 </template>
+
+<script>
+import { mapState } from "pinia";
+import { useModalStore } from "../stores/modal.js";
+
+export default {
+	name: 'AppAuth',
+	computed: {
+		...mapState(useModalStore, ['hiddenClass'])
+	}
+};
+</script>
